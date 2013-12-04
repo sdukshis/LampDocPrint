c5aaaa906b59d93d8f09b7df446ab91400c75676
Pavel Filonov <pfilonov@ptsecurity.ru>
2013-12-04 09:51:33 +0400

    All messages tagget for translation

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.utils.translation import ugettext as _
 # Create your models here.
 
 class Changeable(models.Model):
@@ -10,15 +10,15 @@ class Changeable(models.Model):
 
 
 class Contractor(Changeable):
-    name_eng = models.CharField(verbose_name=u'Название фабрики (англ.)',
+    name_eng = models.CharField(verbose_name=_('Name (eng.)'),
                                      max_length=256)
-    address_eng = models.CharField(verbose_name=u'Адрес (англ.)',
+    address_eng = models.CharField(verbose_name=_('Address (eng.)'),
                                      max_length=512)
-    name_rus = models.CharField(verbose_name=u'Название (рус.)',
+    name_rus = models.CharField(verbose_name=_('Name (rus.)'),
                                      max_length=256)
-    address_rus = models.CharField(verbose_name=u'Адрес (рус.)',
+    address_rus = models.CharField(verbose_name=_('Address (rus.)'),
                                      max_length=512)
-    logo = models.ImageField(verbose_name=u'Логотип', upload_to='logo/')
+    logo = models.ImageField(verbose_name=_('Logo'), upload_to='logo/')
 
     def __unicode__(self):
         return self.name_eng
